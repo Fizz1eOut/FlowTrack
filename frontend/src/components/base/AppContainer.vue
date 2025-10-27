7dd6530f4a188c88756e26a7e5ee29db6d4e94ff
FizzleOut <fizzleout1337@gmail.com>
2025-10-27 20:02:45 +0100

    feat(AppContainer): added responsive universal container component
    - Supports sizes: xs, sm, md, lg, xl
    - Includes adaptive padding for different screen widths
    - Centers content with a max width of 1600px

---
+++ frontend/src/components/base/AppContainer.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+  import { computed } from 'vue';
+
+  const props = withDefaults(
+    defineProps<{
+      size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+    }>(),
+    {
+      size: 'xs',
+    }
+  );
+
+  const container = computed(() => `size-${props.size}`);
+</script>
+
+<template>
+  <div :class="container" class="container">
+    <slot ></slot>
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    max-width: 1600px;
+    margin: 0 auto;
+  }
+  .size-xl {
+    padding: 32px;
+  }
+  .size-lg {
+    padding: 24px;
+  }
+  .size-md {
+    padding: 20px;
+  }
+  .size-sm {
+    padding: 16px;
+  }
+  .size-xs {
+    padding: 12px;
+  }
+  @media (max-width: 768px) {
+    .size-xl {
+      padding: 24px;
+    }
+    .size-lg {
+      padding: 20px;
+    }
+  }
+  @media (max-width: 499px) {
+    .size-xl,
+    .size-lg,
+    .size-md {
+      padding: 16px;
+    }
+    .size-sm {
+      padding: 12px;
+    }
+  }
+</style>
