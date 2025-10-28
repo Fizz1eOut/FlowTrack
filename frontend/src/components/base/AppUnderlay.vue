ec4bcfe793ea023f55dad3308c629a01faa03153
FizzleOut <fizzleout1337@gmail.com>
2025-10-28 19:40:18 +0100

    feat(frontend): added AppUnderlay component
    - Created a reusable wrapper component with background and rounded corners
    - Uses theme variables: --surface and --radius-sm
    - Supports nested content via <slot>

---
+++ frontend/src/components/base/AppUnderlay.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="underlay">
+    <slot ></slot>
+  </div>
+</template>
+
+
+<style scoped>
+  .underlay {
+    background-color: var(--surface);
+    border-radius: var(--radius-sm);
+  }
+</style>
