4e338d55cca5ea06f2c4bc1850bd0ac3a727c8e5
FizzleOut <fizzleout1337@gmail.com>
2025-11-08 18:00:05 +0100

    feat(frontend): enhanced AuthProviders component
    - Added icons (GitHub and Email) using AppIcon component
    - Added hover tooltips indicating feature availability
    - Improved layout and interactivity
    - Added message “We’re working on this feature — stay tuned!” on hover

--- frontend/src/components/content/auth/AuthProviders.vue
+++ frontend/src/components/content/auth/AuthProviders.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
   import AppButton from '@/components/base/AppButton.vue';
+  import AppIcon from '@/components/base/AppIcon.vue';
 </script>
 
 <template>
   <div class="auth-providers">
-    <app-button secondary>Sign In with Google</app-button>
-    <app-button secondary>Sign In with GitHub</app-button>
+    <app-button secondary class="auth-providers__button">
+      <app-icon 
+        name="github"
+        size="var(--fs-xl)"
+        color="var(--color-black)"
+      />
+      Sign In with Google
+    </app-button>
+    <app-button secondary class="auth-providers__button">
+      <app-icon 
+        name="mail"
+        size="var(--fs-xl)"
+        color="var(--color-black)"
+      />
+      Sign In with GitHub
+    </app-button>
   </div>
 </template>
 
@@ -16,5 +31,11 @@
     align-items: center;
     gap: 10px;
     flex-direction: column;
+  }
+  .auth-providers__button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
   }
 </style>
