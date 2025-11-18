c2e5713575e21aec8092d9938fc9853bda6618a7
FizzleOut <fizzleout1337@gmail.com>
2025-11-18 18:15:31 +0100

    feat(frontend): added base AppSearch component
    - Created AppSearch component
    - Added search input with v-model
    - Implemented icon-before slot with search icon
    - Prepared structure for future search functionality

---
+++ frontend/src/components/base/AppSearch.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import AppInput from '@/components/inputs/AppInput.vue';
+  import AppIcon from '@/components/base/AppIcon.vue';
+
+  const searchQuery = ref('');
+</script>
+
+<template>
+  <div class="search">
+    <app-input 
+      v-model="searchQuery"
+      placeholder="Search across all tasks…"
+    >
+      <template v-slot:icon-before>
+        <app-icon
+          name="search"
+          size="var(--fs-md)"
+          color="var(--color-gray)"
+        />
+      </template>
+    </app-input>
+  </div>
+</template>
+
+<style scoped>
+  .search {
+    width: 100%;
+  }
+</style>
