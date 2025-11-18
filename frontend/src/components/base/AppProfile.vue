a7db9e6472e285f3bfd484daa5d71003d7919137
FizzleOut <fizzleout1337@gmail.com>
2025-11-18 18:14:25 +0100

    feat(frontend): added base AppProfile component
    - Created AppProfile component
    - Implemented initial markup and basic styling
    - Prepared structure for further functional enhancements

---
+++ frontend/src/components/base/AppProfile.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="profile">
+    A
+  </div>
+</template>
+
+<style scoped>
+ .profile {
+  font-size: var(--fs-xl);
+  font-weight: var(--fw-normal);
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--primary);
+  border-radius: var(--radius-full);
+  cursor: pointer;
+ }
+</style>
