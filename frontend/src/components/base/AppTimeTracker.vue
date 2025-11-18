4cb87566dcb3b717291a59b3d6952b4022f56401
FizzleOut <fizzleout1337@gmail.com>
2025-11-18 18:13:14 +0100

    feat(frontend): added basic structure for AppTimeTracker component
    - Created AppTimeTracker component
    - Added stopwatch icon using AppIcon
    - Prepared component structure for upcoming time tracking functionality

---
+++ frontend/src/components/base/AppTimeTracker.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import AppIcon from '@/components/base/AppIcon.vue';
+</script>
+
+<template>
+  <div class="time-tracker">
+    <app-icon 
+      name="stopwatch"
+      size="var(--fs-xl)"
+      color="var(--color-dark)"
+    />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
