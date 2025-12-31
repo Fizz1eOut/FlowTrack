449482a19db09879bbb65a413d1f5071bd4b039c
FizzleOut <fizzleout1337@gmail.com>
2025-12-31 16:27:40 +0100

    feat(frontend): add workspace form header
    - Create WorkspaceFormHeader component with title and description
    - Implement form close button

---
+++ frontend/src/components/content/workspace/form/WorkspaceFormHeader.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+  import AppSubtitle from '@/components/base/AppSubtitle.vue';
+  import AppButton from '@/components/base/AppButton.vue';
+  import AppIcon from '@/components/base/AppIcon.vue';
+
+  defineEmits(['close']);
+</script>
+
+<template>
+  <div class="workspace-form__header">
+    <div class="workspace-form__row">
+      <app-subtitle>
+        Create a new Workspace
+      </app-subtitle>
+      <p class="workspace-form__description">Workspace helps organize tasks and projects in one place</p>
+    </div>
+    <app-button @click="$emit('close')" class="workspace-form__btn--cross">
+      <app-icon 
+        name="cross"
+        size="var(--fs-xl)"
+        color="var(--color-gray)"
+      />
+    </app-button>
+  </div>
+</template>
+
+<style scoped>
+  .workspace-form__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    gap: 10px;
+  }
+  .workspace-form__description {
+    font-size: var(--fs-sm);
+    color: var(--color-gray);
+  }
+  .workspace-form__btn--cross {
+    max-width: 34px;
+  }
+</style>
